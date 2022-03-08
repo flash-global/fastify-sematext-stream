9e8b9c24ce10c9a85f7b573344e31a0ad9a8ad5e
glickel <lickel.gaetan@gmail.com>
2022-03-08 16:02:39 +0100

    initial version

---
+++ index.ts
@@ -0,0 +1,75 @@
+import { FastifyLoggerStreamDestination } from 'fastify/types/logger';
+import { symbols } from 'pino';
+import axios from 'axios';
+
+type LogLevel = 10 | 20 | 30 | 40 | 50 | 60;
+
+const loggers = {
+    10: console.trace,
+    20: console.debug,
+    30: console.info,
+    40: console.warn,
+    50: console.error,
+    60: console.error,
+};
+
+const DEFAULT_LOG_LEVEL = 30;
+
+type Config = {
+    baseURL: string,
+    index: string,
+};
+
+type StreamLogger = FastifyLoggerStreamDestination & {
+    [key: symbol]: boolean,
+    lastLevel: LogLevel,
+    config: Config | null,
+};
+
+const sendToSematext = async (msg: string, logLevel: LogLevel, config: Config) => {
+    const url = `${config.baseURL}/${config.index}/${loggers[logLevel].name}`;
+
+    try {
+        await axios.post(url, msg, { headers: { 'Content-Type': 'application/json' } });
+    } catch (error: any) {
+        const content: { [key: string]: any } = {
+            level: 50,
+            type: 'error-request-sematext',
+            message: error?.message,
+        };
+
+        if (error?.isAxiosError) {
+            content.response_data = error.response.data;
+            content.response_status = error.response.status;
+        }
+
+        loggers[50](JSON.stringify(content));
+    }
+};
+
+const stream: StreamLogger = {
+    [symbols.needsMetadataGsym]: true,
+    lastLevel: DEFAULT_LOG_LEVEL,
+    config: null,
+
+    write(msg: string) {
+        if (!this[symbols.needsMetadataGsym]) {
+            return;
+        }
+
+        if (this.config !== null) {
+            setImmediate(() => sendToSematext(msg, this.lastLevel, this.config as Config));
+        }
+
+        loggers[this.lastLevel](msg.trim());
+    },
+};
+
+const build = (
+    config: Config | null = null,
+): StreamLogger => Object.assign<StreamLogger, { config: Config | null, [key: symbol]: boolean, lastLevel: LogLevel }>(
+    stream,
+    { config: config, [symbols.needsMetadataGsym]: true, lastLevel: DEFAULT_LOG_LEVEL },
+);
+
+export { Config, LogLevel, StreamLogger, build };
